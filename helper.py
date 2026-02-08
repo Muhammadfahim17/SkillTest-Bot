d843c4fb8dcac618894a8dcfd7b9f1d118e10b1a
Muhammadfahim17 <fahimbodurov@gmail.com>
2026-02-09 00:28:50 +0500

    update file helper

--- helper.py
+++ helper.py
@@ -18,49 +18,49 @@ QUESTIONS = {
     'Python': {
         'Легкий': [
             {'q': 'Что выведет print(2 + 2)?',
-             'options': ['3', '4', '5', '6'],
-             'correct': 1},
+            'options': ['3', '4', '5', '6'],
+            'correct': 1},
 
             {'q': 'Как создать пустой список?',
-             'options': ['list()', '[]', '{}', '()'],
-             'correct': 1},
+            'options': ['list()', '[]', '{}', '()'],
+            'correct': 1},
 
             {'q': 'Какой тип данных для текста?',
-             'options': ['int', 'str', 'float', 'bool'],
-             'correct': 1},
+            'options': ['int', 'str', 'float', 'bool'],
+            'correct': 1},
 
             {'q': 'Что делает len()?',
-             'options': ['Складывает', 'Возводит в степень', 'Возвращает длину', 'Удаляет'],
-             'correct': 2},
+            'options': ['Складывает', 'Возводит в степень', 'Возвращает длину', 'Удаляет'],
+            'correct': 2},
 
             {'q': 'Какой оператор сравнения?',
-             'options': ['=', '==', '=>', '=<'],
-             'correct': 1},
+            'options': ['=', '==', '=>', '=<'],
+            'correct': 1},
 
             {'q': 'Как проверить равенство?',
-             'options': ['=', '==', 'is', 'equals'],
-             'correct': 1},
+            'options': ['=', '==', 'is', 'equals'],
+            'correct': 1},
 
             {'q': 'Что такое True в Python?',
-             'options': ['Строка', 'Число', 'Логическое значение', 'Список'],
-             'correct': 2},
+            'options': ['Строка', 'Число', 'Логическое значение', 'Список'],
+            'correct': 2},
 
             {'q': 'Какой метод добавляет элемент в список?',
-             'options': ['add()', 'append()', 'push()', 'insert()'],
-             'correct': 1},
+            'options': ['add()', 'append()', 'push()', 'insert()'],
+            'correct': 1},
 
             {'q': 'Что выведет print(type(5))?',
-             'options': [
-                 '&lt;class \"int\"&gt;',
-                 '&lt;class \"str\"&gt;',
-                 '&lt;class \"float\"&gt;',
-                 '&lt;class \"bool\"&gt;'
-             ],
-             'correct': 0},
+            'options': [
+                '&lt;class \"int\"&gt;',
+                '&lt;class \"str\"&gt;',
+                '&lt;class \"float\"&gt;',
+                '&lt;class \"bool\"&gt;'
+            ],
+            'correct': 0},
 
             {'q': 'Какой символ комментария в Python?',
              'options': ['//', '#', '/* */', '--'],
-             'correct': 1},
+            'correct': 1},
         ],
         'Средний': [],
         'Высокий': []
